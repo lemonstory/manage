5c970fb5dfd8d867eb5be6132d6abf972f8aa0c1
microsnow <weixue108@live.cn>
2015-09-06 21:12:47 +0800

    评论采集

--- comment/collection.php
+++ comment/collection.php
@@ -5,61 +5,81 @@ class collection extends controller
 {
     public function action()
     {
-    	$tag     = new Tag();
-    	$album   = new Album();
-        $comment = new Comment();
+        $tag         = new Tag();
+        $album       = new Album();
+        $comment     = new Comment();
+        $commenttask = new CommentTask();
 
-	    if ($_POST) {
-	        $albumid    = (int)$this->getRequest('album_id', 0);
-	    	$source_url = trim($this->getRequest('source_url', ''));
-	    	if (!$albumid) {
-	    		return $this->showErrorJson('album_id-请选择专辑');
-	    	}
-	    	if (!$source_url) {
-	    		return $this->showErrorJson('source_url-请填写页面地址');
-	    	}
-	        $dangdang_id = Http::sub_data($source_url, 'com/', '.html');
-	        if (!is_numeric($dangdang_id)) {
-	        	return $this->showErrorJson('source_url-页面地址不正确');
-	        }
+        if ($_POST) {
+            $albumid    = (int)$this->getRequest('album_id', 0);
+            $source_url = trim($this->getRequest('source_url', ''));
+            if (!$albumid) {
+                return $this->showErrorJson('album_id-请选择专辑');
+            }
+            if (!$source_url) {
+                return $this->showErrorJson('source_url-请填写页面地址');
+            }
+            $dangdang_id = Http::sub_data($source_url, 'com/', '.html');
+            if (!is_numeric($dangdang_id)) {
+                return $this->showErrorJson('source_url-页面地址不正确');
+            }
+            $exists = $commenttask->check_exists("`albumid`={$albumid} and `url`='{$source_url}'");
+            if ($exists) {
+                return $this->showErrorJson('source_url-该评论已经采集');
+            } else {
+                $commenttask->insert(array(
+                    'albumid' => $albumid,
+                    'url'     => $source_url,
+                    'addtime' => date('Y-m-d H:i:s')
+                ));
+            }
 
-	        Http::$referer = $source_url;
+            Http::$referer = $source_url;
 
-	        $page = 1;
+            $page = 1;
 
-	        while (true) {
-	            $url_page = "http://product.dangdang.com/comment/comment.php?product_id={$dangdang_id}&datatype=1&page={$page}&filtertype=2&sysfilter=1&sorttype=1";
+            while (true) {
+                $url_page = "http://product.dangdang.com/comment/comment.php?product_id={$dangdang_id}&datatype=1&page={$page}&filtertype=2&sysfilter=1&sorttype=1";
 
-	            $content = Http::ajax_get($url_page);
-	            $content = json_decode($content, true);
+                $content = Http::ajax_get($url_page);
+                $content = iconv("GBK", "UTF-8", $content);
+                $r = json_decode($content, true);
 
-	            if ($content['data']) {
-	            	foreach($content['data'] as $k => $v) {
-	            		$comment->insert(array(
-			                'albumid' => $albumid,
-			                'content' => $content,
-			                'addtime' => date('Y-m-d H:i:s')
-			            ));
-	            	}
-	            } else {
-	            	break;
-	            }
-	            break;
-	            $page ++;
-	        }
-	        // 标签列表
-	        $taglist = $this->get_tag_from_dangda($source_url);
-	        foreach($taglist as $k => $v) {
-	        	foreach($v as $k2 => $v2) {
-	        		if ($k2) {
-	        			$tag->insert(array(
-	        				'albumid' => $albumid,
-	        				'content' => $v2,
-	        				'addtime' => date('Y-m-d H:i:s')
-	        			));
-	        		}
-	        	}
-	        }
+                if (isset($r['data']) && $r['data']) {
+                    foreach($r['data'] as $k => $v) {
+                        $exists = $comment->check_exists("`albumid`={$albumid} and `content`='{$v['content']}'");
+                        if (!$exists) {
+                            $comment->insert(array(
+                                'albumid' => $albumid,
+                                'content' => $v['content'],
+                                'addtime' => date('Y-m-d H:i:s')
+                            ));
+                        }
+                        
+                    }
+                } else {
+                    break;
+                }
+                $page ++;
+                usleep(100);
+            }
+            // 标签列表
+            $taglist = $this->get_tag_from_dangda($source_url);
+            foreach($taglist as $k => $v) {
+                foreach($v as $k2 => $v2) {
+                    if (in_array($v2, array('童书', '图书'))) {
+                        continue;
+                    }
+                    $exists = $tag->check_exists("`albumid`={$albumid} and `content`='{$v2}'");
+                    if (!$exists) {
+                        $tag->insert(array(
+                            'albumid' => $albumid,
+                            'content' => $v2,
+                            'addtime' => date('Y-m-d H:i:s')
+                        ));
+                    }
+                }
+            }
 
             return $this->showSuccJson('操作成功');
         }
@@ -77,7 +97,7 @@ class collection extends controller
      */
     private function get_tag_from_dangda($url)
     {
-        $content = Http::sub_data($url);
+        $content = Http::get($url);
 
         $content =  @iconv('GBK', 'UTF-8', $content);
         $content = Http::sub_data($content, '<div class="show_info_left">所属分类</div>', '</div>');
