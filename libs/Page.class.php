efa5c188a5eb8a53c477f13b0589f5f3a3acebdf
huqiangqiang <will8436992@sina.com>
2015-10-26 22:45:24 +0800

    edit li css

--- libs/Page.class.php
+++ libs/Page.class.php
@@ -11,7 +11,7 @@ class Page
             $currentPage='<li class="active"><a href="###">' . ($p + 1) . '</a></li>';
             $nextPages='';
             for ($i=$p+1,$end=min(array($page,$p+5));$i<$end;$i++){
-                $nextPages.='<a href="'.$baseUri. '&p=' . $i . '">'.($i+1).'</a>';
+                $nextPages.='<li><a href="'.$baseUri. '&p=' . $i . '">'.($i+1).'</a></li>';
             }
             $nextPage = $p == $page - 1 ? '' : ('<li><a href="'.$baseUri. '&p=' . ($p + 1) . '"aria-label="Next"><span aria-hidden="true">下一页</span></a></li');
             return '<nav><ul class="pagination">' . $lastPage . $previousPages . $currentPage . $nextPages . $nextPage . '</ul>';
