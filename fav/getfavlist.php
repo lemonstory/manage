52497c5efca1f1ef886ed63675df427973f44b14
huqiangqiang <will8436992@sina.com>
2015-10-27 17:33:43 +0800

    add 收藏列表

---
+++ fav/getfavlist.php
@@ -0,0 +1,75 @@
+<?php
+include_once '../controller.php';
+
+class getfavlist extends controller
+{
+    public function action()
+    {
+        $currentPage = $this->getRequest('p') + 0;
+        $perPage = $this->getRequest('perPage', 20) + 0;
+        $searchCondition = $this->getRequest('searchCondition', 'uid');
+        $searchContent = $this->getRequest('searchContent', '');
+        if (empty($currentPage)) {
+            $currentPage = 0;
+        } 
+        if (empty($perPage)) {
+            $perPage = 20;
+        }
+        
+        $pageBanner = "";
+        $baseUri = "/fav/getfavlist.php?perPage={$perPage}&searchCondition={$searchCondition}&searchContent={$searchContent}";
+        $favlist = array();
+        $totalCount = 0;
+        
+        if (!empty($searchContent)) {
+            $columnValue = $searchContent;
+        } else {
+            $column = $columnValue = '';
+        }
+        $managefavobj = new ManageFav();
+        $resultList = $managefavobj->getListByColumnSearch($column, $columnValue, $currentPage + 1, $perPage);
+        if (!empty($resultList)) {
+            $albumids = array();
+            $albumlist = array();
+            foreach ($resultList as $value) {
+                $albumids[] = $value['albumid'];
+            }
+            if (!empty($albumids)) {
+                $albumids = array_unique($albumids);
+                $albumobj = new Album();
+                $albumlist = $albumobj->getListByIds($albumids);
+            }
+            
+            $aliossobj = new AliOss();
+            foreach ($resultList as $value) {
+                $albumid = $value['albumid'];
+                if (empty($albumlist[$albumid])) {
+                    continue;
+                }
+                $albuminfo = $albumlist[$albumid];
+                $albuminfo['cover'] = $aliossobj->getImageUrlNg($albuminfo['cover'], 100);
+                $value['albuminfo'] = $albuminfo;
+                $favlist[] = $value;
+            }
+            
+            $totalCount = $managefavobj->getCountByColumnSearch($column, $columnValue);
+            if ($totalCount > $perPage) {
+                $pageBanner = Page::NumeralPager($currentPage, ceil($totalCount/$perPage), $baseUri, $totalCount);
+            }
+        }
+        
+        $smartyObj = $this->getSmartyObj();
+        $smartyObj->assign('p', $currentPage);
+        $smartyObj->assign('perPage', $perPage);
+        $smartyObj->assign('searchCondition', $searchCondition);
+        $smartyObj->assign('searchContent', $searchContent);
+        $smartyObj->assign('pageBanner', $pageBanner);
+        $smartyObj->assign('favlist', $favlist);
+        $smartyObj->assign('useractive', "active");
+        $smartyObj->assign('getfavlistside', "active");
+        $smartyObj->assign("headerdata", $this->headerCommonData());
+        $smartyObj->display("fav/getfavlist.html"); 
+    }
+}
+new getfavlist();
+?>
---
+++ model/ManageFav.class.php
@@ -0,0 +1,73 @@
+<?php
+class ManageFav extends ModelBase 
+{
+    public $DB_INSTANCE = 'share_main';
+    public $TABLE_NAME = 'fav_album';
+    
+    
+    public function getListByColumnSearch($column = '', $value = '', $currentPage = 1, $perPage = 50)
+    {
+        if (empty($currentPage)) {
+            $currentPage = 1;
+        }
+        if ($currentPage <= 0) {
+            $currentPage = 1;
+        }
+        if (empty($perPage)) {
+            $perPage = 50;
+        }
+        if ($perPage <= 0) {
+            $perPage = 50;
+        }
+        $offset = ($currentPage - 1) * $perPage;
+    
+        $statusWhere = "";
+        $list = $resIds = array();
+        $db = DbConnecter::connectMysql($this->$DB_INSTANCE);
+        $sql = "SELECT * FROM `{$this->$TABLE_NAME}`";
+        if (!empty($column)) {
+            $sql .= " WHERE `{$column}` = '$value'";
+            if (!empty($statusWhere)) {
+                $sql .= " AND {$statusWhere}";
+            }
+        } else {
+            if (!empty($statusWhere)) {
+                $sql .= " WHERE {$statusWhere}";
+            }
+        }
+    
+        $sql .= " ORDER BY `id` DESC LIMIT {$offset}, {$perPage}";
+        $st = $db->prepare($sql);
+        $st->execute();
+        $result = $st->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $result;
+    }
+    
+    
+    public function getCountByColumnSearch($column = '', $value = '')
+    {
+        $statusWhere = "";
+         
+        $db = DbConnecter::connectMysql($this->DB_INSTANCE);
+        $sql = "SELECT COUNT(*) FROM `{$this->TABLE_NAME}`";
+        if (!empty($column)) {
+            $sql .= " WHERE `{$column}` = '$value'";
+            if (!empty($statusWhere)) {
+                $sql .= " AND {$statusWhere}";
+            }
+        } else {
+            if (!empty($statusWhere)) {
+                $sql .= " WHERE {$statusWhere}";
+            }
+        }
+         
+        $st = $db->prepare($sql);
+        $st->execute();
+        $count = $st->fetch(PDO::FETCH_COLUMN);
+        return $count;
+    }
+    
+}
+
+?>
