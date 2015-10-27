0cfd8cce3233a7e43f3b12e3ce131b73e73337be
huqiangqiang <will8436992@sina.com>
2015-10-27 15:48:04 +0800

    edit list css

--- system/hotrecommendlist.php
+++ system/hotrecommendlist.php
@@ -18,7 +18,7 @@ class hotrecommendlist extends controller
         
         $pageBanner = "";
         $baseUri = "/user/hotrecommentlist.php?perPage={$perPage}&searchCondition={$searchCondition}&searchContent={$searchContent}";
-        
+        $totalCount = 0;
     	$hotlist = array();
     	if (!empty($searchContent)) {
             $columnValue = $searchContent;
@@ -28,9 +28,29 @@ class hotrecommendlist extends controller
         $managesysobj = new ManageSystem();
         $resultList = $managesysobj->getRecommendListByColumnSearch("share_main", "recommend_hot", $column, $columnValue, $currentPage + 1, $perPage);
         if (!empty($resultList)) {
+            $albumids = array();
+            $albumlist = array();
             foreach ($resultList as $value) {
-                $hotlist[] = $value;
+                $albumids[] = $value['albumid'];
             }
+            if (!empty($albumids)) {
+                $albumids = array_unique($albumids);
+                $albumobj = new Album();
+                $albumlist = $albumobj->getListByIds($albumids);
+            }
+            
+            $aliossobj = new AliOss();
+            foreach ($resultList as $value) {
+                $albumid = $value['albumid'];
+                if (empty($albumlist[$albumid])) {
+                    continue;
+                }
+                $albuminfo = $albumlist[$albumid];
+                $cominfo = array_merge($albuminfo, $value);
+                $cominfo['cover'] = $aliossobj->getImageUrlNg($cominfo['cover'], 100);
+                $hotlist[] = $cominfo;
+            }
+            var_dump($hotlist);die();
             $totalCount = $managesysobj->getRecommendCountByColumnSearch("share_main", "recommend_hot", $column, $columnValue);
             if ($totalCount > $perPage) {
                 $pageBanner = Page::NumeralPager($currentPage, ceil($totalCount/$perPage), $baseUri, $totalCount);
