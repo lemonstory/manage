3d691c8acbd1b7991ffddc0b2dac19fb48cf9066
huqiangqiang <will8436992@sina.com>
2015-10-28 12:06:09 +0800

    add 搜索关键词统计

---
+++ analytics/searchcontentlist.php
@@ -0,0 +1,54 @@
+<?php
+include_once '../controller.php';
+
+class searchcontentlist extends controller
+{
+    public function action()
+    {
+        $currentPage = $this->getRequest('p') + 0;
+        $perPage = $this->getRequest('perPage', 20) + 0;
+        $status = $this->getRequest('status', 0);
+        $searchCondition = $this->getRequest('searchCondition', 'searchcontent');
+        $searchContent = $this->getRequest('searchContent', '');
+        if (empty($currentPage)) {
+            $currentPage = 0;
+        } 
+        if (empty($perPage)) {
+            $perPage = 20;
+        }
+        
+        $pageBanner = "";
+        $baseUri = "/user/userloginlog.php?perPage={$perPage}&searchCondition={$searchCondition}&searchContent={$searchContent}";
+        $list = array();
+        $totalCount = 0;
+        
+        if (!empty($searchContent)) {
+            $column = $searchCondition;
+            $columnValue = $searchContent;
+        } else {
+            $column = $columnValue = '';
+        }
+        $manageobj = new ManageSearch();
+        $list = $manageobj->getListByColumnSearch($column, $columnValue, $status, $currentPage + 1, $perPage);
+        if (!empty($list)) {
+            $totalCount = $manageobj->getCountByColumnSearch($column, $columnValue, $status);
+            if ($totalCount > $perPage) {
+                $pageBanner = Page::NumeralPager($currentPage, ceil($totalCount/$perPage), $baseUri, $totalCount);
+            }
+        }
+        
+        $smartyObj = $this->getSmartyObj();
+        $smartyObj->assign('p', $currentPage);
+        $smartyObj->assign('perPage', $perPage);
+        $smartyObj->assign('searchCondition', $searchCondition);
+        $smartyObj->assign('searchContent', $searchContent);
+        $smartyObj->assign('pageBanner', $pageBanner);
+        $smartyObj->assign('list', $list);
+        $smartyObj->assign('analyticsactive', "active");
+        $smartyObj->assign('searchcontentlistside', "active");
+        $smartyObj->assign("headerdata", $this->headerCommonData());
+        $smartyObj->display("analytics/searchcontentlist.html"); 
+    }
+}
+new searchcontentlist();
+?>
---
+++ model/ManageSearch.class.php
@@ -0,0 +1,83 @@
+<?php
+class ManageSearch extends ModelBase
+{
+    public $DB_INSTANCE = 'share_main';
+    public $SEARCH_CONTENT_TABLE_NAME = 'search_content_count';
+    
+    
+    public function getListByColumnSearch($column = '', $value = '', $status = 0, $currentPage = 1, $perPage = 50)
+    {
+        if (empty($currentPage)) {
+            $currentPage = 1;
+        }
+        if ($currentPage <= 0) {
+            $currentPage = 1;
+        }
+        if (empty($perPage)) {
+            $perPage = 50;
+        }
+        if ($perPage <= 0) {
+            $perPage = 50;
+        }
+        $offset = ($currentPage - 1) * $perPage;
+        
+        $statusWhere = "";
+        if (!empty($status)) {
+            $statusWhere = "`status` = {$status}";
+        }
+        $list = $resIds = array();
+        $db = DbConnecter::connectMysql($this->DB_INSTANCE);
+        $sql = "SELECT * FROM `{$this->SEARCH_CONTENT_TABLE_NAME}`";
+        if (!empty($column)) {
+            if ($column == 'searchcontent') {
+                $sql .= " WHERE `{$column}` like '%$value'%";
+            } else {
+                $sql .= " WHERE `{$column}` = '$value'";
+            }
+            if (!empty($statusWhere)) {
+                $sql .= " AND {$statusWhere}";
+            }
+        } else {
+            if (!empty($statusWhere)) {
+                $sql .= " WHERE {$statusWhere}";
+            }
+        }
+        
+        $sql .= " ORDER BY `status`, `count` DESC LIMIT {$offset}, {$perPage}";
+        $st = $db->prepare($sql);
+        $st->execute();
+        $result = $st->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $result;
+    }
+    
+    
+    public function getCountByColumnSearch($column = '', $value = '', $status = 0)
+    {
+        $statusWhere = "";
+        if (!empty($status)) {
+            $statusWhere = "`status` = {$status}";
+        }
+        
+        $db = DbConnecter::connectMysql($this->DB_INSTANCE);
+        $sql = "SELECT COUNT(*) FROM `{$this->SEARCH_CONTENT_TABLE_NAME}`";
+        if (!empty($column)) {
+            $sql .= " WHERE `{$column}` = '$value'";
+            if (!empty($statusWhere)) {
+                $sql .= " AND {$statusWhere}";
+            }
+        } else {
+            if (!empty($statusWhere)) {
+                $sql .= " WHERE {$statusWhere}";
+            }
+        }
+         
+        $st = $db->prepare($sql);
+        $st->execute();
+        $count = $st->fetch(PDO::FETCH_COLUMN);
+        return $count;
+    }
+    
+}
+
+?>
