ab06dcbbe4821d5e8e8fc9148416c9b7e2cf8baa
microsnow <weixue108@live.cn>
2015-10-30 15:01:56 +0800

    100

--- daemon/album/cron_uploadOss.php
+++ daemon/album/cron_uploadOss.php
@@ -18,7 +18,7 @@ class cron_uploadOss extends DaemonBase
     protected function uploadOss() {
         // // 更新专辑封面
         $album = new Album();
-        $album_list = $album->get_list("cover=''", 1);
+        $album_list = $album->get_list("cover=''", 100);
 
         foreach ($album_list as $k => $v) {
             $r = $this->middle_upload($v['s_cover'], $v['id'], 1);
@@ -32,7 +32,7 @@ class cron_uploadOss extends DaemonBase
 
         // 更新故事封面
         $story = new Story();
-        $story_list = $album->get_list("cover=''", 1);
+        $story_list = $album->get_list("cover=''", 100);
         foreach ($story_list as $k => $v) {
             $r = $this->middle_upload($v['s_cover'], $v['id'], 2);
             if (is_string($r)) {
@@ -45,7 +45,7 @@ class cron_uploadOss extends DaemonBase
 
         // 更新故事为本地地址
         $story = new Story();
-        $story_list = $story->get_list("mediapath=''", 1);
+        $story_list = $story->get_list("mediapath=''", 100);
         foreach ($story_list as $k => $v) {
             $r = $this->middle_upload($v['source_audio_url'], $v['id'], 3);
             if (is_array($r) && $r) {
