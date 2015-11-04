6d4f3ded35367fc3d174c46ae3bd2865d127edc2
microsnow <weixue108@live.cn>
2015-11-05 00:10:31 +0800

    评论编辑

--- comment/comment_op.php
+++ comment/comment_op.php
@@ -15,6 +15,16 @@ class comment_op extends controller
 		if ($op_name == 'edit_comment') {
 			$value = $this->getRequest('value');
 			$comment->update(array('content' => $value), "`id`={$op_id}");
+		} else if ($op_name == 'edit_star_level') {
+			$value = (int)$this->getRequest('value');
+			if ($value < 0 || $value > 5) {
+				return $this->showErrorJson(ErrorConf::CommentStarLevelIsError());
+			}
+			$comment->update(array('star_level' => $value), "`id`={$op_id}");
+		} else if ($op_name == 'delete') {
+			$comment->update(array('status' => 0), "`id`={$op_id}");
+		} else if ($op_name == 'recover') {
+			$comment->update(array('status' => 1), "`id`={$op_id}");
 		}
 
 		return $this->showSuccJson('操作成功');
