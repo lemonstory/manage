78bb8cde3551577e0b76ef10eb00dd572f5fbb37
microsnow <weixue108@live.cn>
2015-11-05 00:27:24 +0800

    test

--- comment/comment_op.php
+++ comment/comment_op.php
@@ -7,13 +7,14 @@ class comment_op extends controller
     {
         $op_name = $this->getRequest('op_name', '');
         $op_id = $this->getRequest('op_id', 0);
-        if (!$op_id) {
-        	return $this->showErrorJson(ErrorConf::CommentContentIsEmpty());
-        }
+
         $comment = new Comment();
 
-		if ($op_name == 'edit_comment') {
+		if ($op_name == 'edit_content') {
 			$value = $this->getRequest('value');
+			if (!$value) {
+        	return $this->showErrorJson(ErrorConf::CommentContentIsEmpty());
+        }
 			$comment->update(array('content' => $value), "`id`={$op_id}");
 		} else if ($op_name == 'edit_star_level') {
 			$value = (int)$this->getRequest('value');
