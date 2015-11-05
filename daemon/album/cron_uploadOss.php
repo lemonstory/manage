960ba543abab3f64a5fe8d8268e1f8bffb524909
microsnow <weixue108@live.cn>
2015-11-05 14:48:33 +0800

    清除音频同步

--- daemon/album/cron_uploadOss.php
+++ daemon/album/cron_uploadOss.php
@@ -75,28 +75,6 @@ class cron_uploadOss extends DaemonBase
             }
             
         }
-
-        return true;
-        // 更新故事为本地地址
-        $story = new Story();
-        $story_list = $story->get_list("mediapath=''", 500);
-        foreach ($story_list as $k => $v) {
-            $mediapath = $story->get_filed_value('source_audio_url', $v['source_audio_url'], 'mediapath');
-            if ($mediapath) {
-                $story->update(array('mediapath' => $mediapath), "`id`={$v['id']}");
-                $this->writeLog("故事(重复) {$v['id']} => cover 更新成功");
-            } else {
-                $r = $this->middle_upload($v['source_audio_url'], $v['id'], 3);
-                if (is_array($r) && $r) {
-                    $story->update(array('mediapath' => $r['mediapath'], 'times' => $r['times'], 'file_size' => $r['size']), "`id`={$v['id']}");
-                    $this->writeLog("故事 {$v['id']} => mediapath 更新成功");
-                } else {
-                    $this->writeLog("故事 {$v['id']} => mediapath 更新失败");
-                }
-            }
-            
-        }
-
     }
 
     /**
