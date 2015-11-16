ff1408c5b3aa6d9aeccc76951f608374e30921d7
huqiangqiang <will8436992@sina.com>
2015-11-16 14:57:06 +0800

    edit 管理员用户列表

--- privilege/usergroup.php
+++ privilege/usergroup.php
@@ -7,16 +7,16 @@ class usergroup extends controller
     {
         $uid = $this->getRequest('uid');
         $pObj = new ManagePrivilege();
-        /*$userinfo = current($pObj->getUser($uid));
+        $userinfo = current($pObj->getUser($uid));
         $userGroup = $pObj->getUserGroup($uid);
         $groups = $pObj->getGroups();
         foreach ($groups as $key=>$group){
             $gid = $group['id'];
             $groups[$key]['member'] = in_array($gid, $userGroup);
-        }*/
+        }
         $smartyobj = $this->getSmartyObj();
-        //$smartyobj->assign('groups', $groups);
-        //$smartyobj->assign('userinfo', $userinfo);
+        $smartyobj->assign('groups', $groups);
+        $smartyobj->assign('userinfo', $userinfo);
         $smartyobj->assign("privilege", 1);
         $smartyobj->assign("grouplistside", "active");
         $smartyobj->assign("headerdata", $this->headerCommonData());
--- privilege/userlist.php
+++ privilege/userlist.php
@@ -6,11 +6,24 @@ class userlist extends controller
     public function action()
     {
         $page = $this->getRequest('p');
-        $perpage = $this->getRequest('perPage',20);
+        $perpage = $this->getRequest('perPage', 20);
+        $searchCondition = $this->getRequest('searchCondition', 'uid');
+        $searchContent = $this->getRequest('searchContent', '');
+        if (empty($currentPage)) {
+            $currentPage = 0;
+        }
+        if (empty($perPage)) {
+            $perPage = 20;
+        }
+        
+        $pageBanner = "";
+        $baseUri = "/privilege/userlist.php?perPage={$perPage}&searchCondition={$searchCondition}&searchContent={$searchContent}";
+        
         $pObj = new ManagePrivilege();
-        //$userlist = $pObj->getUserList();
+        $userlist = $pObj->getUserList();
+        
         $smartyobj = $this->getSmartyObj();
-        //$smartyobj->assign('userlist', $userlist);
+        $smartyobj->assign('userlist', $userlist);
         $smartyobj->assign('privilege', "active");
         $smartyobj->assign('userlistside', "active");
         $smartyobj->assign("headerdata", $this->headerCommonData());
