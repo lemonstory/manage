d35c89f10f694950f749dd63c71e4cced2b08ec9
huqiangqiang <will8436992@sina.com>
2015-12-08 15:21:21 +0800

    edit 首页统计数据

--- index.php
+++ index.php
@@ -23,27 +23,50 @@ class index extends controller
         }
         $anobj = new Analytics();
         
-        // 今日新增
+        // 今日日统计数据
         $nowdaystime = date("Y-m-d 00:00:00", time());
         $nowdayetime = $nowdaystime + 86400;
-        $favlist = $anobj->getAnalyticsDayList("fav", $nowdaystime, $nowdayetime);
+        $nowdayfavlist = $anobj->getAnalyticsDayList("fav", $nowdaystime, $nowdayetime);
+        $nowdaylistenlist = $anobj->getAnalyticsDayList("listen", $nowdaystime, $nowdayetime);
+        $nowdaycommentlist = $anobj->getAnalyticsDayList("comment", $nowdaystime, $nowdayetime);
+        $nowdayreglist = $anobj->getAnalyticsDayList("reg", $nowdaystime, $nowdayetime);
+        $nowdaydownlist = $anobj->getAnalyticsDayList("down", $nowdaystime, $nowdayetime);
+        
+        // 昨日日统计数据
+        $lastdaystime = $nowdaystime - 86400;
+        $lastdayetime = $nowdaystime;
+        $lastdayfavlist = $anobj->getAnalyticsDayList("fav", $lastdaystime, $lastdayetime);
+        $lastdaylistenlist = $anobj->getAnalyticsDayList("listen", $lastdaystime, $lastdayetime);
+        $lastdaycommentlist = $anobj->getAnalyticsDayList("comment", $lastdaystime, $lastdayetime);
+        $lastdayreglist = $anobj->getAnalyticsDayList("reg", $lastdaystime, $lastdayetime);
+        $lastdaydownlist = $anobj->getAnalyticsDayList("down", $lastdaystime, $lastdayetime);
         
         
+        $nowdayfavcount = $nowdayfavlist['totalnum'];
+        $nowdaylistencount = $nowdayfavlist['totalnum'];
+        $nowdaycommentcount = $nowdayfavlist['totalnum'];
+        $nowdayregcount = $nowdayfavlist['personnum'];
+        $nowdaydowncount = $nowdayfavlist['totalnum'];
         
-        // 新增注册用户数、对比昨天增长百分比、上周对比增长百分比
+        $lastdayfavcount = $lastdayfavlist['totalnum'];
+        $lastdaylistencount = $lastdayfavlist['totalnum'];
+        $lastdaycommentcount = $lastdayfavlist['totalnum'];
+        $lastdayregcount = $lastdayfavlist['personnum'];
+        $lastdaydowncount = $lastdayfavlist['totalnum'];
         
-        // 收听故事量
         
-        // 用户收藏故事专辑量
+        // 计算增长或下降率
+        $rastlist = array();
+        $rastlist['tolastdayfavrast'] = number_format((($nowdayfavcount / $lastdayfavcount) - 1), 2) * 100;
+        $rastlist['tolastdaylistenrast'] = number_format((($nowdaylistencount / $lastdaylistencount) - 1), 2) * 100;
+        $rastlist['tolastdaycommentrast'] = number_format((($nowdaycommentcount / $lastdaycommentcount) - 1), 2) * 100;
+        $rastlist['tolastdayregrast'] = number_format((($nowdayregcount / $lastdayregcount) - 1), 2) * 100;
+        $rastlist['tolastdaydownrast'] = number_format((($nowdaydowncount / $lastdaydowncount) - 1), 2) * 100;
         
-        // 用户下载故事量
-        
-        // 评论总数
         
         // 专辑总数
         
         // 故事总数
-        
         
         // 用户反馈总数
         
@@ -52,6 +75,7 @@ class index extends controller
         $smartyobj = $this->getSmartyObj();
         $smartyobj->assign("indexactive", "active");
         $smartyobj->assign("indexside", "active");
+        $smartyobj->assign("rastlist", $rastlist);
         $smartyobj->assign("headerdata", $this->headerCommonData($uid));
         $smartyobj->display("index.html");
     }
