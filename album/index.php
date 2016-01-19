d5e92a2ee7957d82c8456d741613da115943040f
huqiangqiang <will8436992@sina.com>
2016-01-19 17:49:28 +0800

    edit 后台专辑添加标签展示

--- album/index.php
+++ album/index.php
@@ -45,11 +45,43 @@ class index extends controller
 
         if (count($albumList)) {
             $aliossobj = new AliOss();
+            $albumids = array();
+            foreach ($albumList as $k => $v) {
+                $albumids[] = $v['id'];
+            }
+            // 获取多个专辑的关联tag列表
+            $tagnewobj = new TagNew();
+            $relationlist = $tagnewobj->getAlbumTagRelationListByAlbumIds($albumids);
+            $tagids = array();
+            if (!empty($relationlist)) {
+                foreach ($relationlist as $albumid => $albumtaglist) {
+                    foreach ($albumtaglist as $value) {
+                        $tagids[] = $value['tagid'];
+                    }
+                }
+            }
+            $tagids = array_unique($tagids);
+            // 获取标签信息
+            $taglist = $tagnewobj->getTagInfoByIds($tagids);
+            
             foreach ($albumList as $k => $v) {
                 if ($v['cover']) {
                     $albumList[$k]['cover'] = $aliossobj->getImageUrlNg($aliossobj->IMAGE_TYPE_ALBUM, $v['cover'], 100, $v['cover_time']);
                 } else {
                     $albumList[$k]['cover'] = $v['s_cover'];
+                }
+                // 整合专辑的所有标签
+                $albumtaglist = array();
+                $albumtaglist = $relationlist[$v['id']];
+                if (empty($albumtaglist)) {
+                    continue;
+                }
+                foreach ($albumtaglist as $relationinfo) {
+                    $tagid = $relationinfo['tagid'];
+                    if (empty($taglist[$tagid])) {
+                        continue;
+                    }
+                    $albumList[$k]['taglist'][$tagid] = $taglist[$tagid];
                 }
             }
         }
