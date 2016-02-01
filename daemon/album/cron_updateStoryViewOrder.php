70eb4ddf5f940fe3fd8f4f5de2e0b3cc9b30aaa2
gaoyong <gaoyong06@qq.com>
2016-02-01 23:38:15 +0800

    增加:修复故事排序的业务

--- daemon/album/cron_updateStoryViewOrder.php
+++ daemon/album/cron_updateStoryViewOrder.php
@@ -1,48 +1,82 @@
 <?php
-/**
- * 奇数上传音频进程
- */
+
+//修复故事辑内故事的排序.
+//业务逻辑:
+//  根据故事名修复故事排序值为0(view_order=0),或10000(view_order=10000)的故事排序指
+//  备注:部分view_order=10000是编辑手写的,所有的view_order=0都是系统默认值
+//使用:
+//  修复album_id为4422的所有故事,序号取前3位数:
+//  php your_path/cron_updateStoryViewOrder.php -a 4422 -l 3
+//
+//  修复所有专辑[慎重]:
+//      php your_path/cron_updateStoryViewOrder.php -a all
+
 include_once (dirname ( dirname ( __FILE__ ) ) . "/DaemonBase.php");
 class cron_updateStoryViewOrder extends DaemonBase
 {
     protected $processnum = 1;
+    protected $isWhile = false;
+
     protected function deal() {
-        $this->updateStoryViewOrder();
-        exit;
+
+        set_time_limit(0);
+        $all = "all";
+        $options = getopt("a:l:");
+        $where = "(`view_order`=0 or `view_order`=10000) ";
+
+        $logfile = "/alidata1/cron_updateStoryViewOrder.log";
+        $fp = @fopen($logfile, "a+");
+        $repair_num = 0;
+        $not_required_num = 0;
+        $count = 0;
+
+        if(!empty($options)) {
+
+            $optA = $options['a'];
+            $optL = $options['l'];
+
+        }
+
+        $repair_album_id = intval($optA);
+        $len = intval($optL);
+        if(0 == $repair_album_id && 0 != strcmp($optA,$all)) {
+
+            die("Fail: -a param is incorrect");
+
+        }
+
+        if($repair_album_id > 0) {
+
+            $where .= "and `album_id` = {$repair_album_id}";
+        }
+
+        $story_obj = new Story();
+        $story_list = $story_obj->get_filed_list("`id`,`title`", $where);
+
+        foreach ($story_list as $story) {
+
+            $count++;
+            $view_order = $story_obj->get_view_order($story['title'],$len);
+            $view_order = intval($view_order);
+            if(0 != $view_order) {
+
+                $repair_num++;
+                $story_obj->update(array('view_order' => $view_order), "`id`={$story['id']}");
+                fwrite($fp, "getStoryViewOrder: title: {$story['title']}, id: {$story['id']}, view_order: {$view_order}\n");
+            }else {
+
+                $not_required_num++;
+                //避免为修复的一直排在最前面,将序号更改为ID
+                $story_obj->update(array('view_order' => $story['id']), "`id`={$story['id']}");
+                fwrite($fp, "doNothing: title: {$story['title']}, id: {$story['id']}, view_order: {$view_order}\n");
+            }
+            $story_obj->clearStoryCache($story['id']);
+        }
+        $story_obj->clearAlbumStoryListCache($repair_album_id);
+        fwrite($fp,"Done! count:{$count}, repairNum:{$repair_num},  notRequiredNum:{$not_required_num}\n");
+        fclose($fp);
     }
 
     protected function checkLogPath() {}
-
-
-    protected function updateStoryViewOrder() {
-        set_time_limit(0);
-        $story = new Story();
-        $story_list = $story->get_filed_list("`id`,`title`", " `view_order`=0");
-        foreach ($story_list as $k => $v) {
-            $r = $story->get_view_order($v['title'], 1);
-            if (!$r) {
-                $r = $story->get_view_order($v['title'], 2);
-            }
-            $r = intval($r);
-            if ($r) {
-                if ($r) {
-                    // $story->update(array('view_order' => $r), "`id`={$v['id']}");
-                    $this->writeLog("故事ID:{$v['id']} title:{$v['title']} 更新为 {$r}");
-                    echo "故事ID:{$v['id']} title:{$v['title']} 更新为 {$r}\n";
-                    sleep(1);
-                }
-            }
-        }
-    }
-
-    protected function writeLog($content = '')
-    {
-        static $manageCollectionCronLog = null;
-        if (!$manageCollectionCronLog) {
-            $manageCollectionCronLog = new ManageCollectionCronLog();
-        }
-        $manageCollectionCronLog->insert(array('type' => 'update_story_view_order', 'content' => $content));
-        
-    }
 }
 new cron_updateStoryViewOrder();
