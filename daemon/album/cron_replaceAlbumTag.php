77d77d5cb139dbe100a71d4e47344f6b8336facc
gaoyong <gaoyong06@qq.com>
2016-09-20 17:47:39 +0800

    增加父标签的处理

--- daemon/album/cron_replaceAlbumTag.php
+++ daemon/album/cron_replaceAlbumTag.php
@@ -3,25 +3,41 @@
 //
 //业务逻辑:
 //      将所有故事辑中的标签A,替换为标签B
-//
-//使用:
-//      将所有故事专辑中的标签A替换为标签B
+//用法一: 将所有故事专辑中的标签A替换为标签B
 //
 //  php your_path/cron_replaceAlbumTag.php -f find_tag_A_id -r replace_tag_B_id
 //
-//儿童英语->英语
-//  php ./cron_replaceAlbumTag.php -f 129 -r 4
-//儿童科普->科普
-//  php ./cron_replaceAlbumTag.php -f 130 -r 9
-//TODO:是否需要定期执行?
+//少儿英语	->	英语  php ./cron_replaceAlbumTag.php -f 159 -r 4
+//儿歌童谣	->	儿歌  php ./cron_replaceAlbumTag.php -f 160 -r 1
+//亲子读物	->	育儿  php ./cron_replaceAlbumTag.php -f 162 -r 153
+//育儿宝典	->	育儿  php ./cron_replaceAlbumTag.php -f 163 -r 153
+//经典童话   -> 	童话寓言 php ./cron_replaceAlbumTag.php -f 24 -r 158
+//童话故事   -> 	童话寓言 php ./cron_replaceAlbumTag.php -f 126 -r 158
+//经典寓言   -> 	童话寓言 php ./cron_replaceAlbumTag.php -f 26 -r 158
+//
+//每周日23:00分执行
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 159 -r 4 >> /dev/null &
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 160 -r 1 >> /dev/null &
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 162 -r 153 >> /dev/null &
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 163 -r 153 >> /dev/null &
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 24 -r 158 >> /dev/null &
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 126 -r 158 >> /dev/null &
+//0 2 * * * /alidata/server/php/bin/php /alidata1/www/htdocs/manage.xiaoningmeng.net/daemon/album/cron_replaceAlbumTag.php -f 26 -r 158 >> /dev/null &
 
-include_once (dirname ( dirname ( __FILE__ ) ) . "/DaemonBase.php");
 
-class cron_replaceAlbumTag extends DaemonBase {
+//用法二: 添加父级标签
+//给所有{童话寓言}的专辑增加{故事}标签 php ./cron_replaceAlbumTag.php -f 158 -r 158
+
+
+include_once(dirname(dirname(__FILE__)) . "/DaemonBase.php");
+
+class cron_replaceAlbumTag extends DaemonBase
+{
 
     protected $isWhile = false;
 
-    protected function deal() {
+    protected function deal()
+    {
 
         $options = getopt("f:r:");
         $logfile = "/alidata1/cron_replaceAlbumTag.log";
@@ -30,7 +46,7 @@ class cron_replaceAlbumTag extends DaemonBase {
         $not_required_num = 0;
         $count = 0;
 
-        if(!empty($options)) {
+        if (!empty($options)) {
 
             $optF = $options['f'];
             $optR = $options['r'];
@@ -39,12 +55,12 @@ class cron_replaceAlbumTag extends DaemonBase {
         $find_tag_id = intval($optF);
         $replace_tag_id = intval($optR);
 
-        if(0 == $find_tag_id || 0 == $replace_tag_id) {
+        if (0 == $find_tag_id || 0 == $replace_tag_id) {
 
             die("Fail: -f or -r param is incorrect");
         }
 
-        if(!empty($find_tag_id) && !empty($replace_tag_id)) {
+        if (!empty($find_tag_id) && !empty($replace_tag_id)) {
 
             $startRelationId = 0;
             $direction = "down";
@@ -54,28 +70,39 @@ class cron_replaceAlbumTag extends DaemonBase {
             $ret = false;
             $tagNewObj = new TagNew();
 
-            while($isFirstLoop || $albumTagRelationListCount > 0 ) {
+            while ($isFirstLoop || $albumTagRelationListCount > 0) {
 
                 $isFirstLoop = false;
-                $albumTagRelationList = $tagNewObj->getAlbumTagRelationListFromTag($find_tag_id,0,0,0,$direction,$startRelationId,$section);
+                $albumTagRelationList = $tagNewObj->getAlbumTagRelationListFromTag($find_tag_id, 0, 0, 0, $direction, $startRelationId, $section);
                 $albumTagRelationListCount = count($albumTagRelationList);
-                $lastIndex = $albumTagRelationListCount - 1 ;
-                $startRelationId = $albumTagRelationList[$lastIndex]['id'];
-                if(!empty($albumTagRelationList) && is_array($albumTagRelationList)) {
 
-                    foreach($albumTagRelationList as $item) {
+                if ($albumTagRelationListCount > 0) {
+                    $lastIndex = $albumTagRelationListCount - 1;
+                    $startRelationId = $albumTagRelationList[$lastIndex]['id'];
+                    if (!empty($albumTagRelationList) && is_array($albumTagRelationList)) {
 
-                        $count++;
-                        $albumid = $item['albumid'];
-                        $tagid = $item['tagid'];
-                        $update_data = array('tagid'=>$replace_tag_id);
-                        $ret = $tagNewObj->updateAlbumTagRelationInfo($albumid, $tagid, $update_data);
-                        if($ret) {
-                            $repair_num++;
-                            fwrite($fp, "replaceAlbumID: {$albumid} find_tag_id: {$tagid} replace_tag_id: {$replace_tag_id}  ret: {$ret}\n");
+                        foreach ($albumTagRelationList as $item) {
+
+                            $count++;
+                            $albumid = $item['albumid'];
+                            $tagid = $item['tagid'];
+                            $update_data = array('tagid' => $replace_tag_id);
+                            $ret = $tagNewObj->updateAlbumTagRelationInfo($albumid, $tagid, $update_data);
+
+                            //获取父标签,并将父标签添加至专辑
+                            $replace_tag_info = $tagNewObj->getTagInfoByIds($replace_tag_id);
+                            if (0 != $replace_tag_info[$replace_tag_id]['pid']) {
+                                $ret = $tagNewObj->addAlbumTagRelationInfo($albumid, $replace_tag_info[$replace_tag_id]['pid']);
+                            }
+
+                            if ($ret) {
+                                $repair_num++;
+                                fwrite($fp, "replaceAlbumID: {$albumid} find_tag_id: {$tagid} replace_tag_id: {$replace_tag_id}  ret: {$ret}\n");
+                            }
                         }
                     }
-               }
+                }
+
             }
         }
 
@@ -83,7 +110,10 @@ class cron_replaceAlbumTag extends DaemonBase {
         fclose($fp);
     }
 
-    protected function checkLogPath() {}
+    protected function checkLogPath()
+    {
+    }
 
 }
+
 new cron_replaceAlbumTag ();
