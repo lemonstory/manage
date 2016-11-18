d28a911c056f4b0f914ffc44f8a9c62ca173b0f4
jack02.zhang <jack02.zhang@vipshop.com>
2016-11-18 15:40:07 +0800

    分页功能

--- comment/ddInfoList.php
+++ comment/ddInfoList.php
@@ -12,10 +12,19 @@ class ddInfoList extends controller
 {
     public function action()
     {
+        $currentPage = $this->getRequest('p') + 0;
+        $perPage     = $this->getRequest('perPage', 20) + 0;
         $title     = $this->getRequest('title', '');
         $author     = $this->getRequest('author', '');
+        $baseUri = "/comment/ddInfoList.php?perPage={$perPage}";
 
         $searchFilter = $where = array();
+        if (empty($currentPage)) {
+            $currentPage = 0;
+        }
+        if (empty($perPage)) {
+            $perPage = 20;
+        }
         if(!empty($title)){
             $where['title'] = $title;
             $searchFilter['title'] = $title;
@@ -27,10 +36,18 @@ class ddInfoList extends controller
         $manageCollectionDdLog = new ManageCollectionDdLog();
         
         $totalCount = $manageCollectionDdLog->getDdInfoTotalCount($where);
-        $ddList = $manageCollectionDdLog->getDdInfoList($where);
+        $ddList = $manageCollectionDdLog->getDdInfoList($where,$currentPage + 1, $perPage);
+        
+        $pageBanner = "";
+        if ($totalCount > $perPage) {
+            $pageBanner = Page::NumeralPager($currentPage, ceil($totalCount/$perPage), $baseUri, $totalCount);
+        }
         
         $smartyObj = $this->getSmartyObj();
         $smartyObj->assign('totalCount', $totalCount);
+        $smartyObj->assign('p', $currentPage);
+        $smartyObj->assign('perPage', $perPage);
+        $smartyObj->assign('pageBanner', $pageBanner);
         $smartyObj->assign('ddList', $ddList);
         $smartyObj->assign('searchFilter', $searchFilter);
         $smartyObj->assign('ddinfolistactive', "active");
